2064df5fc52d6c223cd8bb7801513c7d1d1e1cfa
Brian Le <brianle1301@gmail.com>
2019-12-17 21:52:12 +0700

    [workspace] Supports only cjs atm with polyfills

--- babel.config.js
+++ babel.config.js
@@ -1,30 +1,17 @@
-const browserOpts = {
-  modules: 'umd',
-  targets: {
-    ie: '11',
-    edge: '16',
-    firefox: '68',
-    safari: '12',
-    chrome: '70',
-    opera: '60',
-  },
-};
-
-const nodeOpts = {
-  modules: 'cjs',
-  targets: {
-    node: '8',
-  },
-};
-
-const enhancedOpts = process.env.BABEL_ENV === 'umd' ? browserOpts : nodeOpts;
-
 const config = {
   presets: [
     [
       '@babel/preset-env',
       {
-        ...enhancedOpts,
+        targets: {
+          ie: '11',
+          edge: '16',
+          firefox: '68',
+          safari: '12',
+          chrome: '70',
+          opera: '60',
+          node: '8',
+        },
         corejs: 3,
         useBuiltIns: 'usage',
       },
@@ -34,6 +21,6 @@ const config = {
   ignore: ['node_modules'],
 };
 
-if (process.env.BABEL_ENV !== 'development') config.ignore.push('src/__tests__');
+if (process.env.BABEL_ENV === 'production') config.ignore.push('src/__tests__');
 
 module.exports = config;
