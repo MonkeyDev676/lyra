89ce0f55d419a667d041b73ada8c641ebf1d39c7
Brian Le <brianle1301@gmail.com>
2020-02-08 15:33:03 +0000

    [core] Tweak toHaveBeenCalledWith

--- src/__test__/utils.js
+++ src/__test__/utils.js
@@ -3,6 +3,6 @@ const equal = require('@botbind/dust/src/equal');
 module.exports = {
   toHaveBeenCalledTimes: (mock, times) => mock.mock.calls.length === times,
   toHaveBeenCalled: mock => mock.mock.calls.length >= 1,
-  toHaveBeenCalledWith: (mock, { args, equalOpts = {} }) =>
+  toHaveBeenCalledWith: (mock, { args, equalOpts }) =>
     mock.mock.calls.some(callArgs => equal(callArgs, args, equalOpts)),
 };
