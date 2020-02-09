e3ceaeb935a90947067256171c3b7ed3bd69180a
Brian Le <brianle1301@gmail.com>
2020-02-09 09:48:19 +0000

    [core] Add date schema

---
+++ src/__test__/schemas/date.test.js
@@ -0,0 +1,80 @@
+const equal = require('@botbind/dust/src/equal');
+const date = require('../../schemas/date');
+const State = require('../../State');
+const utils = require('../utils');
+
+const proto = Object.getPrototypeOf(date);
+const state = new State();
+const d = new Date('12/31/2019');
+const d2 = new Date('01/01/2020');
+const d3 = new Date('01/02/2020');
+
+describe('date', () => {
+  describe('Validate', () => {
+    it('should validate correctly', () => {
+      expect(date.validate(d).value).toBe(d);
+
+      utils.spy(() => date.validate('x'), {
+        proto,
+        method: '$createError',
+        args: ['date.base', state, {}, undefined],
+      });
+    });
+  });
+
+  describe('Coerce', () => {
+    it('should coerce strings to dates', () => {
+      expect(equal(date.validate('12/31/2019', { strict: false }).value, d)).toBe(true);
+      expect(equal(date.validate(d.getTime(), { strict: false }).value, d)).toBe(true);
+
+      utils.spy(() => date.validate('x', { strict: false }), {
+        proto,
+        method: '$createError',
+        args: ['date.coerce', state, {}, undefined],
+      });
+    });
+  });
+
+  ['max', 'min', 'greater', 'smaller'].forEach(method => {
+    const [valid, invalid] = method === 'max' || method === 'smaller' ? [d, d3] : [d3, d];
+
+    describe(`date.${method}()`, () => {
+      [d2, 'now'].forEach(input => {
+        const isNow = input === 'now';
+
+        it(`should validate against ${isNow ? 'now' : 'a fixed date'}`, () => {
+          if (isNow) {
+            const original = Date.now;
+
+            Date.now = () => d2.getTime();
+
+            Date.now.restore = () => {
+              Date.now = original;
+            };
+          }
+
+          const schema = date[method](input);
+
+          expect(schema.validate(valid).value).toBe(valid);
+
+          if (method === 'max' || method === 'min') expect(schema.validate(d2).value).toBe(d2);
+
+          if (method === 'greater' || method === 'smaller')
+            utils.spy(() => schema.validate(d2), {
+              proto,
+              method: '$createError',
+              args: [`date.${method}`, state, {}, { date: input }],
+            });
+
+          utils.spy(() => schema.validate(invalid), {
+            proto,
+            method: '$createError',
+            args: [`date.${method}`, state, {}, { date: input }],
+          });
+
+          if (isNow) Date.now.restore();
+        });
+      });
+    });
+  });
+});
---
+++ src/schemas/date.js
@@ -0,0 +1,109 @@
+const compare = require('@botbind/dust/src/compare');
+const BaseSchema = require('./BaseSchema');
+
+function _isValidDate(value) {
+  return value instanceof Date && !Number.isNaN(value.getTime());
+}
+
+module.exports = new BaseSchema().define({
+  type: 'date',
+  messages: {
+    'date.base': '{label} must be a valid date',
+    'date.coerce': '{label}} cannot be coerced to a date object',
+    'date.max': '{label} must be smaller than or equal to {date}',
+    'date.min': '{label} must be greater than or equal to {date}',
+    'date.greater': '{label} must be greater than {date}',
+    'date.smaller': '{label} must be smaller than {date}',
+  },
+
+  coerce: (value, helpers) => {
+    if (typeof value === 'number') {
+      value = new Date(value);
+
+      if (_isValidDate(value)) return { value, errors: null };
+    }
+
+    if (typeof value === 'string') {
+      const timestamp = Date.parse(value);
+
+      if (!Number.isNaN(timestamp)) {
+        return { value: new Date(timestamp), errors: null };
+      }
+    }
+
+    return { value: null, errors: [helpers.createError('date.coerce')] };
+  },
+
+  validate: (value, helpers) => {
+    // Check for invalid dates
+    if (_isValidDate(value)) return { value, errors: null };
+
+    return { value: null, errors: [helpers.createError('date.base')] };
+  },
+
+  rules: {
+    compare: {
+      method: false,
+      validate: (value, helpers) => {
+        let date;
+
+        if (helpers.params.date === 'now') date = Date.now();
+        else date = helpers.params.date.getTime();
+
+        if (compare(value.getTime(), date, helpers.params.operator)) return { value, errors: null };
+
+        return {
+          value: null,
+          errors: helpers.createError(`date.${helpers.name}`, { date: helpers.params.date }),
+        };
+      },
+      params: [
+        {
+          name: 'date',
+          assert: resolved => resolved === 'now' || resolved instanceof Date,
+          reason: 'must be now or an instance of Date',
+        },
+      ],
+    },
+
+    min: {
+      method(date) {
+        return this.$addRule({
+          name: 'min',
+          method: 'compare',
+          params: { date, operator: '>=' },
+        });
+      },
+    },
+
+    max: {
+      method(date) {
+        return this.$addRule({
+          name: 'max',
+          method: 'compare',
+          params: { date, operator: '<=' },
+        });
+      },
+    },
+
+    greater: {
+      method(date) {
+        return this.$addRule({
+          name: 'greater',
+          method: 'compare',
+          params: { date, operator: '>' },
+        });
+      },
+    },
+
+    smaller: {
+      method(date) {
+        return this.$addRule({
+          name: 'smaller',
+          method: 'compare',
+          params: { date, operator: '<' },
+        });
+      },
+    },
+  },
+});
