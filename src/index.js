0c0cd098a590f3ef8d3c4a9e7a6b2a6bf1a7a211
Brian Le <brianle1301@gmail.com>
2020-03-28 13:39:23 +0000

    [core] Allow built-in schemas to be extended

--- src/index.js
+++ src/index.js
@@ -1,67 +1,83 @@
 /* eslint-disable global-require */
 const assert = require('@botbind/dust/src/assert');
 const attachMethod = require('@botbind/dust/src/attachMethod');
-const alternatives = require('./schemas/alternatives');
-const boolean = require('./schemas/boolean');
-const string = require('./schemas/string');
-const number = require('./schemas/number');
-const array = require('./schemas/array');
-const func = require('./schemas/function');
-const object = require('./schemas/object');
+const Any = require('./any');
+
+const _types = {
+  any: Any.any,
+  alternatives: require('./schemas/alternatives'),
+  boolean: require('./schemas/boolean'),
+  string: require('./schemas/string'),
+  date: require('./schemas/date'),
+  number: require('./schemas/number'),
+  array: require('./schemas/number'),
+  function: require('./schemas/function'),
+  object: require('./schemas/object'),
+};
 
 const root = {
   ...require('./identities'),
-  ...require('./any'),
   ...require('./ref'),
+
   symbols: require('./symbols'),
-  alternatives,
-  alt: alternatives,
-  boolean,
-  bool: boolean,
-  string,
-  str: string,
-  date: require('./schemas/date'),
-  number,
-  num: number,
-  array,
-  arr: array,
-  func,
-  function: func,
-  object,
-  obj: object,
+  isSchema: Any.isSchema,
+  _types: new Set(Object.keys(_types)),
+
+  ..._types,
+
+  // Methods
   attempt: (schema, value, opts) => {
     return schema.attempt(value, opts);
   },
   validate: (schema, value, opts) => {
     return schema.validate(value, opts);
   },
-  extend(...definitions) {
-    const Lyra = { ...this };
+  extend: (...extensions) => {
+    const Lyra = { ...root };
 
-    for (let definition of definitions) {
-      if (typeof definition === 'function') definition = definition(Lyra);
+    // Clone types
+    Lyra._types = new Set(Lyra._types);
+
+    assert(extensions.length > 0, 'The parameter extensions must contain at least an extension');
+
+    for (let extension of extensions) {
+      if (typeof extension === 'function') extension = extension(Lyra);
 
       assert(
-        Lyra[definition.type] === undefined,
-        'The option type for extend is invalid as it is built-in',
+        Lyra[extension.type] === undefined || Lyra._types.has(extension.type),
+        'Invalid extension',
+        extension.type,
       );
 
-      definition = {
-        from: this.any,
-        ...definition,
-      };
+      assert(
+        extension.from === undefined || this.isSchema(extension.from),
+        'The option from for extend must be a valid schema',
+      );
 
-      assert(this.isSchema(definition.from), 'The option from for extend must be a valid schema');
+      const from = extension.from === undefined ? Lyra.any : extension.from;
 
-      const from = definition.from;
+      Lyra[extension.type] = from.extend(extension);
 
-      Lyra[definition.type] = from.extend(definition);
+      Lyra._types.add(extension.type);
     }
 
     return Lyra;
   },
 };
 
+// Aliases
+for (const [type, alias] of [
+  ['boolean', 'bool'],
+  ['string', 'str'],
+  ['alternatives', 'alt'],
+  ['number', 'num'],
+  ['function', 'func'],
+  ['array', 'arr'],
+  ['object', 'obj'],
+])
+  root[alias] = root[type];
+
+// Shortcut
 for (const methodName of [
   'annotate',
   'custom',
